438582fd1748642a42e26794a5f99fd52ea533be
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-01-21 12:18:43 +0900

    improve command.sh

--- bin/command.sh
+++ bin/command.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 
 bin_dir=`dirname $0`
-root_dir=`cd ${bin_dir}; cd ../;pwd`
+bundle_root_dir=`cd ${bin_dir}; cd ../;pwd`
 
-cd ${root_dir}
+cd ${bundle_root_dir}
 cd ../../../
 
+root_dir=`pwd`
+tmp_dir=tmp/zipcode
 URL=http://www.post.japanpost.jp/zipcode/dl
 
-rm -f ken_all.zip jigyosyo.zip
-wget ${URL}/kogaki/zip/ken_all.zip
-wget ${URL}/jigyosyo/zip/jigyosyo.zip
+if [ ! -d ${tmp_dir} ]; then
+    mkdir -p ${tmp_dir}
+fi
 
+# tmp dir
+cd ${tmp_dir}
+
+rm -rf *
+
+# home zipcode file
+wget ${URL}/kogaki/zip/ken_all.zip
 unzip ken_all.zip
+
+if [ ! -f KEN_ALL.CSV ]; then
+    exit 1
+fi
+
+iconv -f cp932 -t utf8 KEN_ALL.CSV > ken_all_iconv.csv
+
+# office zipcode file
+wget ${URL}/jigyosyo/zip/jigyosyo.zip
 unzip jigyosyo.zip
 
-iconv -f cp932 -t utf8 ken_all.csv > ken_all_iconv.csv
-iconv -f cp932 -t utf8 jigyosyo.csv > jigyosyo_iconv.csv
+if [ ! -f JIGYOSYO.CSV ]; then
+    exit 1
+fi
 
-php app/console contrib:japan-zipcode:home-zipcode-fixture -f ken_all_iconv.csv $*
-php app/console contrib:japan-zipcode:office-zipcode-fixture -f jigyosyo_iconv.csv $*
+iconv -f cp932 -t utf8 JIGYOSYO.CSV > jigyosyo_iconv.csv
 
-rm -f ken_all.csv ken_all_iconv.csv jigyosyo.csv jigyosyo_iconv.csv ken_all.zip jigyosyo.zip
+# prj root
+cd ${root_dir}
+
+php app/console contrib:japan-zipcode:home-zipcode-fixture -f ${tmp_dir}/ken_all_iconv.csv $*
+php app/console contrib:japan-zipcode:office-zipcode-fixture -f ${tmp_dir}/jigyosyo_iconv.csv $*
